1c2ded09e81c1b0bbf2ab64afef352ace1fa2ad1
Carlos Gonzalez <cyberpolin@gmail.com>
2016-09-15 13:17:31 -0500

    Add general mockup, and start timer from 25min

--- Pomodoro/index.ios.js
+++ Pomodoro/index.ios.js
@@ -9,38 +9,98 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 class Pomodoro extends Component {
+  constructor(props){
+    super(props)
+    this.state = {timeLeft: 0}
+  }
+  toggleTimer(){
+    var currentTime = 1500 // 25 min
+    var min = function(time){
+      return Math.floor(time/60)
+    }
+    var seg = function(time){
+      return time%60
+    }
+
+    setInterval(()=>{
+      currentTime--
+      this.setState({timeLeft: String(min(currentTime))+':'+String(seg(currentTime))})
+
+    }, 1000)
+
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Nothing else
-        </Text>
+      <View style={styles.view}>
+        <CheckList/>
+        <MainContainer time={this.state.timeLeft} toggleTimer={this.toggleTimer.bind(this)}/>
+        <Adds/>
       </View>
     );
   }
 }
+class CheckList extends Component{
+  render(){
+    return(
+      <View style={styles.checkList}>
+        <Text>check list </Text>
+      </View>
+    )
+  }
+}
+
+class MainContainer extends Component{
+  render(){
+    return(
+
+      <TouchableOpacity onPress={this.props.toggleTimer} style={styles.mainContainer}>
+        <Text>{String(this.props.time)}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+class Adds extends Component{
+  render(){
+    return(
+      <View style={styles.adds}>
+        <Text>Beautiful money maker adds</Text>
+      </View>
+    )
+  }
+}
+
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  checkList: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  mainContainer: {
+    flex: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
   },
+  adds: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  }
 });
 
 AppRegistry.registerComponent('Pomodoro', () => Pomodoro);
